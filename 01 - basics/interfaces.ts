47d4328e64f0852f2541341c271e1d6c05c60648
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2023-01-08 13:04:52 +0100

    08-01-2023

--- 01 - basics/interfaces.ts
+++ 01 - basics/interfaces.ts
@@ -2,3 +2,31 @@
 /*
 Interface creates a new type, describing the property names and value types of an object.
 */
+interface Reportable {
+  summary(): string;
+}
+
+const car1 = {
+  name: 'Civic',
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
+};
+
+const drink1 = {
+  color: 'Brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar.`;
+  },
+};
+
+const printSummary = (obj: Reportable): void => {
+  console.log(obj.summary());
+};
+
+printSummary(car1);
+printSummary(drink1);
