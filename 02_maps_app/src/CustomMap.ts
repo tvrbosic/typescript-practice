6d3f60f79e91580a8e2fdd8af6b3c71527cda01f
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2023-04-19 11:06:14 +0200

    19_04_2023

--- 02_maps_app/src/CustomMap.ts
+++ 02_maps_app/src/CustomMap.ts
@@ -1,3 +1,12 @@
+// Instructions to every other class on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -8,6 +17,23 @@ export class CustomMap {
         lat: 0,
         lng: 0,
       },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
--- 02_maps_app/src/index.ts
+++ 02_maps_app/src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * IMPORTANT: This is a TypeScript project, so we need to compile it to JavaScript.
+ * Application can be tested in browser with Parcel: https://parceljs.org/
+ * npx parcel 02_maps_app/src/index.html
+ */
+
 import { User } from './User';
 import { Company } from './Company';
 import { CustomMap } from './CustomMap';
@@ -5,7 +11,10 @@ import { CustomMap } from './CustomMap';
 const user = new User();
 const company = new Company();
 
-new CustomMap('map');
+const customMap = new CustomMap('map');
 
 console.log(user);
 console.log(company);
+
+customMap.addMarker(user);
+customMap.addMarker(company);
