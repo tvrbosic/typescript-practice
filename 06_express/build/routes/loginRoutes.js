9f5e4ba13e37a93ea4d320b02cbeedf094081fab
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2023-07-29 17:18:13 +0200

    29_07_2023

--- 06_express/build/routes/loginRoutes.js
+++ 06_express/build/routes/loginRoutes.js
@@ -2,6 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
+function requireAuthentication(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Unauthorized!');
+}
 const router = (0, express_1.Router)();
 exports.router = router;
 router.get('/login', (req, res) => {
@@ -23,5 +31,38 @@ router.post('/login', (req, res) => {
     const { email, password } = req.body;
     console.log(`Email: ${email}`);
     console.log(`Password: ${password}`);
-    res.send('Login successful!');
+    if (email && password && email === 'user@email.com' && password === 'Password1234') {
+        // Mark this person as logged in
+        req.session = { loggedIn: true };
+        // Redirect them to the root route
+        res.redirect('/');
+    }
+    else {
+        res.send('Invalid email or password');
+    }
+});
+router.get('/', (req, res) => {
+    if (req.session && req.session.loggedIn) {
+        res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+    }
+    else {
+        res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+    }
+});
+router.get('/logout', (req, res) => {
+    req.session = undefined;
+    res.redirect('/');
+});
+router.get('/protected', requireAuthentication, (req, res) => {
+    res.send('Welcome to protected route, logged in user!');
 });
--- 06_express/src/index.ts
+++ 06_express/src/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['test'] }));
 app.use(router);
 
 app.listen(3000, () => {
