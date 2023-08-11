c9d0382081132ab06cfba2b93a536dd9727272ea
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2023-08-11 14:00:16 +0200

    11_08_2023

--- 06_express/src/controllers/LoginController.ts
+++ 06_express/src/controllers/LoginController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
+import { get, controller } from './decorators';
 
+@controller('/auth')
 class LoginController {
+  @get('/login')
   getLogin(req: Request, res: Response): void {
     res.send(`
         <form method="POST">
